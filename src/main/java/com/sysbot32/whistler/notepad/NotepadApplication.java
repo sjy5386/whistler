d6d77b2af9341ff1b0aebc50ffe3a9a9fec36804
Seokjin Yoon <sjy5386@gmail.com>
2023-02-18 23:55:07 +0900

    Add NotepadApplication class

---
+++ src/main/java/com/sysbot32/whistler/notepad/NotepadApplication.java
@@ -0,0 +1,7 @@
+package com.sysbot32.whistler.notepad;
+
+public class NotepadApplication {
+    public static void main(String[] args) {
+        new NotepadFrame(new Notepad()).setVisible(true);
+    }
+}
