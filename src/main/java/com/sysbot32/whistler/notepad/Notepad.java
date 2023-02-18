d19bc5f74c65b211868482d75e1704ca8c576862
Seokjin Yoon <sjy5386@gmail.com>
2023-02-18 23:47:21 +0900

    Add Notepad class

---
+++ src/main/java/com/sysbot32/whistler/notepad/Notepad.java
@@ -0,0 +1,30 @@
+package com.sysbot32.whistler.notepad;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Getter
+public class Notepad {
+    @Setter
+    private String content = "";
+    private Path path = null;
+
+    public void createNew() {
+        this.content = "";
+        this.path = null;
+    }
+
+    public String open(final Path path) throws IOException {
+        this.content = Files.readString(path);
+        this.path = path;
+        return this.content;
+    }
+
+    public void save(final Path path) throws IOException {
+        Files.writeString(path, this.content);
+    }
+}
