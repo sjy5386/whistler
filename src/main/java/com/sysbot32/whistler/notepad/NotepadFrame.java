1dc7d99b12b5df692119dc4eb1c94a4f72021547
Seokjin Yoon <sjy5386@gmail.com>
2023-02-18 23:53:45 +0900

    Update NotepadFrame

--- src/main/java/com/sysbot32/whistler/notepad/NotepadFrame.java
+++ src/main/java/com/sysbot32/whistler/notepad/NotepadFrame.java
@@ -3,21 +3,23 @@ package com.sysbot32.whistler.notepad;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class NotepadFrame extends JFrame {
+    private final Notepad notepad;
     private final JTextArea textArea;
 
-    public NotepadFrame() {
-        super("Notepad");
+    public NotepadFrame(final Notepad notepad) {
+        this.notepad = notepad;
+
+        this.setTitle("Notepad");
         this.setSize(1280, 720);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         final JPanel contentPane = (JPanel) this.getContentPane();
         contentPane.setLayout(new BorderLayout());
 
-        this.textArea = new JTextArea();
+        this.textArea = new JTextArea(notepad.getContent());
         final JScrollPane scrollPane = new JScrollPane(this.textArea);
 
         this.setJMenuBar(this.createMenuBar());
@@ -42,17 +44,8 @@ public class NotepadFrame extends JFrame {
         final JMenuItem saveAsMenuItem = new JMenuItem("Save As");
         final JMenuItem exitMenuItem = new JMenuItem("Exit");
 
-        newMenuItem.addActionListener(e -> this.textArea.setText(""));
-        openMenuItem.addActionListener(e -> {
-            final JFileChooser fileChooser = new JFileChooser();
-            if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-                try {
-                    this.textArea.setText(Files.readString(Paths.get(fileChooser.getSelectedFile().toURI())));
-                } catch (final IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
+        newMenuItem.addActionListener(e -> this.createNew());
+        openMenuItem.addActionListener(e -> this.open());
         exitMenuItem.addActionListener(e -> System.exit(0));
 
         fileMenu.add(newMenuItem);
@@ -76,5 +69,23 @@ public class NotepadFrame extends JFrame {
         formatMenu.add(fontMenuItem);
 
         return formatMenu;
+    }
+
+    private void createNew() {
+        this.notepad.createNew();
+        this.textArea.setText(this.notepad.getContent());
+    }
+
+    private void open() {
+        final JFileChooser fileChooser = new JFileChooser();
+        if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            try {
+                this.notepad.open(Paths.get(fileChooser.getSelectedFile().toURI()));
+            } catch (final IOException ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                this.textArea.setText(this.notepad.getContent());
+            }
+        }
     }
 }
